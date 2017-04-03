969b961fc809fd440ad236c6d48767c42b443ddb
meo84 <oopsiday@gmail.com>
2017-04-04 01:55:03 +0200

    fix temperature unit in main file comment

--- main.go
+++ main.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "strings"
-  "os"
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-  mw := multiWeatherProvider{
-    openWeatherMap{},
-    weatherUnderground{apiKey: os.Getenv("WG_API_KEY")},
-  }
+	mw := multiWeatherProvider{
+		openWeatherMap{},
+		weatherUnderground{apiKey: os.Getenv("WG_API_KEY")},
+	}
 
-  http.HandleFunc("/weather/", weather(mw))
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/weather/", weather(mw))
+	http.ListenAndServe(":8080", nil)
 }
 
 func weather(mw multiWeatherProvider) func(w http.ResponseWriter, r *http.Request) {
-  return func(w http.ResponseWriter, r *http.Request) {
-    city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	return func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-    temp, err := mw.temperature(city)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
+		temp, err := mw.temperature(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    json.NewEncoder(w).Encode(map[string]interface{}{
-    "city": city,
-    "temp": temp,
-  })
-  }
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"city": city,
+			"temp": temp,
+		})
+	}
 }
 
 type weatherProvider interface {
-  temperature(city string) (float64, error) //in Kelvin
+	temperature(city string) (float64, error) //in Celsius
 }
 
 type multiWeatherProvider []weatherProvider
 
 func (w multiWeatherProvider) temperature(city string) (float64, error) {
-  temps := make(chan float64, len(w))
-  errs := make(chan error, len(w))
+	temps := make(chan float64, len(w))
+	errs := make(chan error, len(w))
 
-  for _, provider := range w {
-    go func(p weatherProvider) {
-      k, err := p.temperature(city)
-      if err != nil {
-        errs <- err
-        return
-      }
-      temps <- k
-    }(provider)
-  }
+	for _, provider := range w {
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
+	}
 
-  sum := 0.0
+	sum := 0.0
 
-  for i := 0; i < len(w); i++ {
-    select {
-    case temp := <-temps:
-      sum += temp
-    case err := <-errs:
-      return 0, err
-    }
-  }
-  return sum / float64(len(w)), nil
+	for i := 0; i < len(w); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
+			return 0, err
+		}
+	}
+	return sum / float64(len(w)), nil
 }
